5e0c2d86f4727eb8e1a41506f834735eeb59ab83
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-10-13 22:53:12 +0600

    admin update

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,19 +1,262 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+
 from .models import (
-    Slider, Service, Item,
-    Doctor, Expertize, Faq,
-    Gallery, Contact, Speciality
+    Slider, Speciality, Service, Item, Doctor, Expertize, Faq, Gallery,
+    Contact
 )
 
-admin.site.register(Slider)
-admin.site.register(Service)
-admin.site.register(Item)
-admin.site.register(Doctor)
-admin.site.register(Expertize)
-admin.site.register(Faq)
-admin.site.register(Gallery)
-admin.site.register(Contact)
-admin.site.register(Speciality)
+
+@admin.register(Slider)
+class SliderAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'order',
+        'created_at',
+        'is_active',
+        'is_deleted',
+        'caption',
+        'slogan',
+        'image',
+        'phone_number',
+        'service_page_url',
+    )
+    list_filter = (
+        'created_user',
+        'update_user',
+        'deleted_user',
+        'created_at',
+        'updated_At',
+        'is_active',
+        'is_deleted',
+    )
+    date_hierarchy = 'created_at'
+    list_editable = ('order', 'is_active', 'is_deleted', )
+
+
+@admin.register(Speciality)
+class SpecialityAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'order',
+        'created_at',
+        'is_active',
+        'is_deleted',
+        'name',
+    )
+    list_filter = (
+        'created_user',
+        'update_user',
+        'deleted_user',
+        'created_at',
+        'updated_At',
+        'is_active',
+        'is_deleted',
+    )
+    search_fields = ('name',)
+    date_hierarchy = 'created_at'
+    list_editable = ('order', 'is_active', 'is_deleted', )
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'order',
+        'created_at',
+        'is_active',
+        'is_deleted',
+        'title',
+        'description',
+        'thumbnail',
+        'cover',
+        'image1',
+        'image2',
+    )
+    list_filter = (
+        'created_user',
+        'update_user',
+        'deleted_user',
+        'created_at',
+        'updated_At',
+        'is_active',
+        'is_deleted',
+    )
+    raw_id_fields = ('items',)
+    date_hierarchy = 'created_at'
+    list_editable = ('order', 'is_active', 'is_deleted', )
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+       'id',
+        'order',
+        'created_at',
+        'is_active',
+        'is_deleted',
+        'title',
+    )
+    list_filter = (
+        'created_user',
+        'update_user',
+        'deleted_user',
+        'created_at',
+        'updated_At',
+        'is_active',
+        'is_deleted',
+    )
+    date_hierarchy = 'created_at'
+    list_editable = ('order', 'is_active', 'is_deleted', )
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'order',
+        'created_at',
+        'is_active',
+        'is_deleted',
+        'user',
+        'name',
+        'doctor_id',
+        'speciality',
+        'picture',
+        'details',
+        'present_hospital',
+        'twitter',
+        'facebook',
+        'instagram',
+        'division',
+        'district',
+        'upazila',
+        'thana',
+        'post_code',
+        'address',
+    )
+    list_filter = (
+        'created_user',
+        'update_user',
+        'deleted_user',
+        'created_at',
+        'updated_At',
+        'is_active',
+        'is_deleted',
+        'user',
+        'speciality',
+        'division',
+        'district',
+        'upazila',
+        'thana',
+    )
+    raw_id_fields = ('expertize',)
+    search_fields = ('name',)
+    date_hierarchy = 'created_at'
+    list_editable = ('order', 'is_active', 'is_deleted', )
+
+
+@admin.register(Expertize)
+class ExpertizeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'order',
+        'created_at',
+        'is_active',
+        'is_deleted',
+        'name',
+    )
+    list_filter = (
+        'created_user',
+        'update_user',
+        'deleted_user',
+        'created_at',
+        'updated_At',
+        'is_active',
+        'is_deleted',
+    )
+    search_fields = ('name',)
+    date_hierarchy = 'created_at'
+    list_editable = ('order', 'is_active', 'is_deleted', )
+
+
+@admin.register(Faq)
+class FaqAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'order',
+        'created_at',
+        'is_active',
+        'is_deleted',
+        'question',
+        'answer',
+    )
+    list_filter = (
+        'created_user',
+        'update_user',
+        'deleted_user',
+        'created_at',
+        'updated_At',
+        'is_active',
+        'is_deleted',
+    )
+    date_hierarchy = 'created_at'
+    list_editable = ('order', 'is_active', 'is_deleted', )
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'order',
+        'created_at',
+        'is_active',
+        'is_deleted',
+        'title',
+        'image',
+    )
+    list_filter = (
+        'created_user',
+        'update_user',
+        'deleted_user',
+        'created_at',
+        'updated_At',
+        'is_active',
+        'is_deleted',
+    )
+    date_hierarchy = 'created_at'
+    list_editable = ('order', 'is_active', 'is_deleted', )
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'order',
+        'created_at',
+        'is_active',
+        'is_deleted',
+        'name',
+        'email',
+        'phone',
+        'subject',
+        'message',
+        'response',
+    )
+    list_filter = (
+        'created_user',
+        'update_user',
+        'deleted_user',
+        'created_at',
+        'updated_At',
+        'is_active',
+        'is_deleted',
+        'response',
+    )
+    search_fields = ('name',)
+    date_hierarchy = 'created_at'
+    list_editable = ('is_active', 'is_deleted', )
+
 
 admin.site.site_header = 'PMS Admin'
 admin.site.site_title = 'PM-System'

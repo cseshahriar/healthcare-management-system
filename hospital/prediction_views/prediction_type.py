885729c3e42837daf090c522e117cc1bc64c5b7d
cseshahriar <cse.shahriar.hosen@gmail.com>
2024-10-17 21:01:29 +0600

    visit count fix

--- hospital/prediction_views/prediction_type.py
+++ hospital/prediction_views/prediction_type.py
@@ -15,4 +15,13 @@ class PredictionType(TemplateView):
     model = DoctorAppointment
     template_name = 'dashboard/prediction/type.html'
 
+    def get_context_data(self, **kwargs):
+        kwargs.setdefault('view', self)
+        if self.extra_context is not None:
+            kwargs.update(self.extra_context)
 
+        qs = DoctorAppointment.objects.filter(
+            doctor__user=self.request.user, is_visited=True
+        )
+        kwargs['visit_count'] = qs.count()
+        return kwargs
